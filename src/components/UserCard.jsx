cc8d43367bee5d14d335fd86290ae3445773b658
Andrew Parra <nixonandrewparra@gmail.com>
2023-08-22 23:20:51 -0500

    alerts

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,35 @@
 const UserCard = ({ user, deleteUser, handleClickUpdateUser }) => {
   return (
-    <article className="  flex flex-col justify-center  px-2  border-2 rounded-sm text-start :w-[381px] ">
-      <ul className=" flex flex-col gap-1  text-lg font-lato text-black/70 text-justify justify-between items-start  hover:justify-betwee ">
-        <li className="flex flex-row  text-center text-2xl font-lora pt-2  border-b-2 border-[#8fff8b44]  ">{`${user.first_name} ${user.last_name}`}</li>
+    <article className="  flex flex-col justify-center mt-2  bg-white px-2  border-2 rounded-sm text-start  h-full  max-w-sm">
+      <ul className=" flex flex-col gap-2  text-lg font-lato text-black/70  justify-between items-start  hover:justify-betwee ">
+        <li className="flex flex-grow w-full  text-center text-3xl font-lora pt-2  border-b-2 border-black/10">{`${user.first_name} ${user.last_name}`}</li>
         <li className="flex-col flex">
-          <span className="font-light text-sm text-teal-50/6">Email</span>{" "}
+          <span className="font-bold w-full text-sm text-slate-200">Email</span>
           {user.email}
         </li>
-        <li className="flex-col flex border-b-2 border-[#8fff8b44] ">
-          <span className="font-light text-sm text-teal-50/60">Birthday</span>{" "}
+        <li className="flex-col flex  w-full  justify-center items-center border-b-2 border-black/5 ">
+          <span className="font-bold text-sm text-slate-200 ">
+            <i class="bx bx-gift text-black "></i> Birthday
+          </span>
           {user.birthday}
         </li>
       </ul>
-      <div className=" flex  items-end justify-end text-end gap-2 p-2">
-        <button onClick={() => deleteUser(user.id)} className="flex-row">
-          delete
+      <div className=" flex   items-end justify-end text-end gap-3 p-2 ite">
+        <button
+          onClick={() => deleteUser(user.id)}
+          className="flex-row bg-red-600 p-[4px] h-[30px] w-[28px]  items-center justify-center rounded-lg text-white border-2 border-red-700 hover:bg-red-500 shadow-sm shadow-red-700  "
+        >
+          <i className="bx bx-trash items-center justify-center flex "></i>
         </button>
-        <button onClick={() => handleClickUpdateUser(user)}>save</button>
+        <button
+          className="flex-row bg-slate-300 p-[4px]  h-[30px] w-[28px] items-center justify-center rounded-lg text-white border-2 border-slate-400 hover:bg-slate-200 shadow-m shadow-slate-700 "
+          onClick={() => handleClickUpdateUser(user)}
+        >
+          <i className="bx bx-edit-alt items-center justify-center flex "></i>
+        </button>
       </div>
     </article>
   );
 };
-69;
 
 export default UserCard;
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -2,7 +2,7 @@ import UserCard from "./UserCard";
 
 const UserList = ({ users, deleteUser, handleClickUpdateUser }) => {
   return (
-    <section className="flex flex-col gap-2 px-4 py-2">
+    <section className="grid grid-cols-1 justify-center sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 px-4 py-2 w-full h-full">
       {users.map((user) => (
         <UserCard
           key={user.id}
